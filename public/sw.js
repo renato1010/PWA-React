9023ae5ac45056f73adea51e38cc0207eb5a6801
Renato Perez <renatoperezc@gmail.com>
2018-05-04 11:26:31 -0600

    se creo el script basico de sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,1 +1,17 @@
-console.log('Service worker running');
+const VERSION = '1.0';
+
+// install event
+
+self.addEventListener('install', () => {
+  console.log('[Service-Worker] Install');
+});
+
+// activate event
+self.addEventListener('activate', () => {
+  console.log('[Service-Worker] Activate');
+});
+
+// fetch event
+self.addEventListener('fetch', event => {
+  console.log('[Service-Worker] fetch', event.request);
+});
