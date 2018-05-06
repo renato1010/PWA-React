066f77c40ef976e1541f964a81bc294dde0ed743
renato <renatoperezc@gmail.com>
2018-05-06 00:58:56 -0600

    se finalizo la implementacion del cache con service workers

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,71 @@
-const VERSION = '1.0';
+const CACHE_NAME = 'V7';
 
+const STATIC_PATHS = ['/', '/offline.html', '/assets/favicon-32x32.png', '/assets/favicon-16x16.png'];
 // install event
 
-self.addEventListener('install', () => {
+self.addEventListener('install', installEvent => {
   console.log('[Service-Worker] Install');
+  installEvent.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      fetch('asset-manifest.json').then(response => {
+        if (response.ok) {
+          response.json().then(manifest => {
+            const urls = Object.keys(manifest).map(key => manifest[key]);
+            urls.push(...STATIC_PATHS);
+            cache.addAll(urls);
+          });
+        }
+      });
+    })
+  );
 });
 
 // activate event
-self.addEventListener('activate', () => {
+self.addEventListener('activate', activateEvent => {
   console.log('[Service-Worker] Activate');
+  activateEvent.waitUntil(
+    caches.keys().then(keyList => {
+      Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
 });
 
 // fetch event
-self.addEventListener('fetch', event => {
-  // console.log('[Service-Worker] fetch', event.request);
+self.addEventListener('fetch', fetchEvent => {
+  const request = fetchEvent.request;
+  fetchEvent.respondWith(
+    caches
+      .match(request)
+      .then(response => {
+        return (
+          response ||
+          fetch(request)
+            .then(res => {
+              caches.open(CACHE_NAME).then(cache => {
+                cache.put(request, res);
+              });
+              return res.clone();
+            })
+            .catch(err => {
+              console.error('[SW] cache in fetch error');
+              return caches.open(CACHE_NAME).then(cache => {
+                if (request.headers.get('accept').includes('text/html')) {
+                  return cache.match('/offline.html');
+                }
+                if (request.url.match(/\.(jpe?g|png|gif|svg)$/)) {
+                  return cache.match('/assets/android-chrome-256x256.png');
+                }
+              });
+            })
+        );
+      })
+      .catch(error => console.log(error))
+  );
 });
